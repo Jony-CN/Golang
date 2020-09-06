d3eae2763bef8fb3730485e71489fa630d7cd70d
jonycna@163.com <jonycna@163.com>
2020-09-06 23:43:07 +0800

    200906

---
+++ src/go_code/project_02/one/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	// fmt.Println("hello\tworld!")
+
+	// fmt.Println("hello:\"world!\"")
+
+	// fmt.Println("h\r我在最前面\r第一位")
+
+	fmt.Println("姓名\t年龄\t籍贯\t住址\nJony\t12\t河北\t北京")
+}
