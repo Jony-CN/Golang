7dd52cc7163ca12b5a26c02faf75c2fd37974c59
Jony <jonycna@163.com>
2020-09-07 17:56:52 +0800

    2020907

--- src/go_code/project_01/main/myFirst.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main(){
-	
-	fmt.Println("我的第一个 go 程序")
-	
-}
---
+++ src/go_code/project_03/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	
+)
+
+func main()  {
+
+	var c3 = "中国"
+	
+	fmt.Printf(c3)
+
+}
