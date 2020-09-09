55ed01cbf8e3e5ef5f142fedee1165516816c12c
jonycna@163.com <jonycna@163.com>
2020-09-09 23:51:06 +0800

    20200909

--- src/go_code/project_03/main.go
+++ src/go_code/project_03/main.go
@@ -6,12 +6,11 @@ import (
 
 func main()  {
 
-	var n1 int32 = 12
+	var n1 int = 10
 
-	var n3 int8
-
-	n3 = int8(n1) + 127
-	fmt.Println(n3)
-
+	fmt.Println(&n1)
+	
+	var ptr *int = &n1
+	fmt.Println(ptr)
 	
 }
